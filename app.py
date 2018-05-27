e9292de0f7b3acdc5b3f9fba3b9f0365d9b585df
ramosjanoah <ramosj.noah@gmail.com>
2018-05-27 13:19:04 +0700

    Edit

--- app.py
+++ app.py
@@ -17,8 +17,18 @@ with open('channel_access_token') as fi:
 	cat = text[0]
 	secret = text[1]
 
+print(cat)
+print(secret)
+
 line_bot_api = LineBotApi(cat)
 handler = WebhookHandler(secret)
+
+@app.route("/darling", methods=['GET'])
+def darling():
+	text = "Oh darling, your request is {}<br>".format(request)
+	text += "LINE BOT API : {}<br>".format(line_bot_api)
+	text += "Secret[0:10] : {}<br>".format(secret[0:10])
+	return text
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -41,7 +51,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text="Apapun yang kamu katakan, aku akan reply pake pesan ini."))
 
 
 if __name__ == "__main__":
